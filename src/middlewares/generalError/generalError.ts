9d9da64045cf81cad18d08448ce8c06712654ec9
Dani Setien <dani.setien@gmail.com>
2023-02-25 13:28:16 +0100

    Add extensions to imports on generalError

--- src/middlewares/generalError/generalError.ts
+++ src/middlewares/generalError/generalError.ts
@@ -1,6 +1,6 @@
 import { type NextFunction, type Request, type Response } from "express";
 import createDebug from "debug";
-import { type CustomError } from "../../customError/customError";
+import { type CustomError } from "../../customError/customError.js";
 
 export const debug = createDebug("sn:error");
 
